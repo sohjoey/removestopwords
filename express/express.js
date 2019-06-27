3fcd150ad541016b01c3cc09036de9717444a70d
sohjoey <joeysoh@outlook.com>
2019-06-27 12:47:28 +0800

    Update express.js

--- express/express.js
+++ express/express.js
@@ -36,9 +36,6 @@ app.use('/testPost',cors(corsOption),function(req,res){
     collection.insertMany([
       {a : 1}, {a : 2}, {a : 3}
     ], function(err, result) {
-      assert.equal(err, null);
-      assert.equal(3, result.result.n);
-      assert.equal(3, result.ops.length);
       console.log("Inserted 3 documents into the collection");
       res.json(result);
     });
