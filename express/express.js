aadda810bfa9b0b67a56cdfc801caaf722d93554
sohjoey <joeysoh@outlook.com>
2019-06-27 12:29:37 +0800

    Update express.js

--- express/express.js
+++ express/express.js
@@ -27,7 +27,6 @@ app.use('/testPost',cors(corsOption),function(req,res){
   MongoClient.connect(url, function(err, client) {
     console.log(`url: ${url}`)
     console.log(`err: ${err}`)
-    console.log(`client: ${JSON.stringify(client)}`)
     res.json('test')
   })
 })
